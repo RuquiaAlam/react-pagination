2b237db8a3f9fdd284dfaabff482381472f1f20f
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-10-10 13:24:48 -0700

    updated heading

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import logo from './logo.svg';
-import {useState} from "react";
+import { useState } from "react";
 import ReactPaginate from "react-paginate";
+import "./App.css";
+import JsonData from "./MOCK_DATA.json";
 
-import './App.css';
-import JsonData from "./MOCK_DATA.json"
 function App() {
-  const [users,setUsers]=useState(JsonData.slice(0,50));
-  const[pageNumber,setPageNumber]=useState(0);
-  const usersPerPage=10;
+  const [users, setUsers] = useState(JsonData.slice(0, 50));
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 10;
 
-  const pagesVisited=pageNumber*usersPerPage;
- //40->50
-  const displayUsers= users.slice(pagesVisited,pagesVisited+usersPerPage).map((user)=>
-     {
-      return(
+  const pagesVisited = pageNumber * usersPerPage;
+  //40->50
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return (
         <div className="user">
           <h3>{user.firstName}</h3>
           <h3>{user.lastName}</h3>
           <h3>{user.email}</h3>
         </div>
-      )
-     })
-     const pageCount=Math.ceil(users.length/usersPerPage);
-     const changePage=({selected})=>
-     {
-      setPageNumber(selected);
-
-     }
+      );
+    });
+  const pageCount = Math.ceil(users.length / usersPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
   return (
     <div className="App">
+      <h1>React Pagination Implementation </h1>
       {displayUsers}
       <ReactPaginate
         previousLabel={"Previous"}
